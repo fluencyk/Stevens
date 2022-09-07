a98cf0259774ea7bc45f232bc8b91a85c87b4151
Yujun Kong <96102053+fluencyk@users.noreply.github.com>
2022-09-07 16:16:28 +0800

    Add files via upload

---
+++ Majoring/SSW-567_A/Assignments/Homework_00/hw_00_YujunKong-SSW567.py
@@ -0,0 +1,49 @@
+# ===== Python - UTF-8 ===== #
+"""/*
+School: Stevens Institute of Technology
+Course: SSW 567 - A
+Instructor: Prof. Andre Bondi
+---------------------------------------
+Topic: Homework # 01
+---------------------------------------
+Coder with CWID: Yujun Kong / 1046 6820
+*/"""
+# ===== CODING BEGINS ===== #
+
+import sys
+
+# <class>
+class SayHello:
+
+    # ([constructor])
+    def __init__(self) -> None:
+        
+        self.helloSaying: str = "Hello world!"
+
+    # ((method))
+    def say(self) -> None:
+
+        print(f"--= ' {self.helloSaying} ' =--")
+
+# </>
+
+# (((main)))
+def main():
+
+    s = SayHello()
+    s.say()
+
+    sys.exit()
+
+"""///*** main program executes below ***///"""
+
+if __name__ == '__main__':
+    main()
+
+# ===== CODING ENDS ===== #
+
+# /// The Coding Experience and Conclusion ///
+"""/'
+Nope...
+'/"""
+# ///
